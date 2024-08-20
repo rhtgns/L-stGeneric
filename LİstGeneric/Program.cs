8bcbbd57fd3285dbacd2e799e61ebde863528e13
rohat güneş <rohatgns44@gmail.com>
2024-08-20 16:46:25 +1000

    Add project files.

---
+++ LİstGeneric/Program.cs
@@ -0,0 +1,22 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+
+        List<string> invitess = new List<string>();
+        invitess.Add("Bülent Ersoy");
+        invitess.Add("Ajda Pekkan");
+        invitess.Add("Ebru Gündeş");
+        invitess.Add("\"Hadise");
+        invitess.Add("Hande Yenery");
+        invitess.Add("Tarkan");
+        invitess.Add("Funda Arar");
+        invitess.Add("Demet Akalın");
+
+        foreach (string i in invitess)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
